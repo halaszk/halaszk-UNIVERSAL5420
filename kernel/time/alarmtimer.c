c0086170d541a3ab804c9b910f02de1ee2b83eb5
John Stultz <john.stultz@linaro.org>
2012-09-17 23:12:53 -0400

    alarmtimer: Use hrtimer per-alarm instead of per-base
    Arve Hjønnevåg reported numerous crashes from the
    "BUG_ON(timer->state != HRTIMER_STATE_CALLBACK)" check
    in __run_hrtimer after it called alarmtimer_fired.
    
    It ends up the alarmtimer code was not properly handling
    possible failures of hrtimer_try_to_cancel, and because
    these faulres occur when the underlying base hrtimer is
    being run, this limits the ability to properly handle
    modifications to any alarmtimers on that base.
    
    Because much of the logic duplicates the hrtimer logic,
    it seems that we might as well have a per-alarmtimer
    hrtimer, and avoid the extra complextity of trying to
    multiplex many alarmtimers off of one hrtimer.
    
    Thus this patch moves the hrtimer to the alarm structure
    and simplifies the management logic.
    
    Cc: Arve Hjønnevåg <arve@android.com>
    Cc: Colin Cross <ccross@google.com>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Reported-by: Arve Hjønnevåg <arve@android.com>
    Signed-off-by: John Stultz <john.stultz@linaro.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    
    Conflicts:
    	kernel/time/alarmtimer.c

--- kernel/time/alarmtimer.c
+++ kernel/time/alarmtimer.c
@@ -181,14 +181,15 @@ static enum hrtimer_restart alarmtimer_fired(struct hrtimer *timer)
 	int restart = ALARMTIMER_NORESTART;
 
 	spin_lock_irqsave(&base->lock, flags);
-	alarmtimer_dequeue(base, alarm);
-		spin_unlock_irqrestore(&base->lock, flags);
 
-		if (alarm->function)
+	alarmtimer_remove(base, alarm);
+	spin_unlock_irqrestore(&base->lock, flags);
+
+	if (alarm->function)
 		restart = alarm->function(alarm, base->gettime());
 
 	spin_lock_irqsave(&base->lock, flags);
-		if (restart != ALARMTIMER_NORESTART) {
+	if (restart != ALARMTIMER_NORESTART) {
 		hrtimer_set_expires(&alarm->timer, alarm->node.expires);
 		alarmtimer_enqueue(base, alarm);
 		ret = HRTIMER_RESTART;
@@ -340,7 +341,7 @@ int alarm_try_to_cancel(struct alarm *alarm)
 	spin_lock_irqsave(&base->lock, flags);
 	ret = hrtimer_try_to_cancel(&alarm->timer);
 	if (ret >= 0)
-		alarmtimer_dequeue(base, alarm);
+		alarmtimer_remove(base, alarm);
 	spin_unlock_irqrestore(&base->lock, flags);
 	return ret;
 }
