22d70b6c4660fa19de146ee239edcc3165377213
Frederic Weisbecker <fweisbec@gmail.com>
2013-06-19 23:56:22 +0200

    sched: Consolidate open coded preemptible() check
    preempt_schedule() open code their preemptability checks.
    
    Use the standard API instead for consolidation.
    
    Signed-off-by: Frederic Weisbecker <fweisbec@gmail.com>
    Cc: Ingo Molnar <mingo@kernel.org>
    Cc: Li Zhong <zhong@linux.vnet.ibm.com>
    Cc: Paul E. McKenney <paulmck@linux.vnet.ibm.com>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Steven Rostedt <rostedt@goodmis.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Alex Shi <alex.shi@intel.com>
    Cc: Paul Turner <pjt@google.com>
    Cc: Mike Galbraith <efault@gmx.de>
    Cc: Vincent Guittot <vincent.guittot@linaro.org>
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -3649,13 +3649,11 @@ int mutex_can_spin_on_owner(struct mutex *lock)
  */
 asmlinkage void __sched notrace preempt_schedule(void)
 {
-	struct thread_info *ti = current_thread_info();
-
 	/*
 	 * If there is a non-zero preempt_count or interrupts are disabled,
 	 * we do not want to preempt the current task. Just return..
 	 */
-	if (likely(ti->preempt_count || irqs_disabled()))
+	if (likely(!preemptible()))
 		return;
 
 	do {
