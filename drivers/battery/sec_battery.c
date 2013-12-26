5a799cf8caf84952fb45b9456a777f43eab45ca6
halaszk <halaszk@deathlord.eu>
2013-12-26 01:33:44 +0100

    battery: Properly populate current now, avg, and max output properties

--- drivers/battery/sec_battery.c
+++ drivers/battery/sec_battery.c
@@ -81,6 +81,7 @@ static enum power_supply_property sec_battery_props[] = {
 	POWER_SUPPLY_PROP_VOLTAGE_AVG,
 	POWER_SUPPLY_PROP_CURRENT_NOW,
 	POWER_SUPPLY_PROP_CURRENT_AVG,
+	POWER_SUPPLY_PROP_CURRENT_MAX,
 	POWER_SUPPLY_PROP_CHARGE_NOW,
 	POWER_SUPPLY_PROP_CAPACITY,
 	POWER_SUPPLY_PROP_TEMP,
@@ -1494,6 +1495,20 @@ static void sec_bat_get_battery_info(
 		POWER_SUPPLY_PROP_VOLTAGE_AVG, value);
 	battery->voltage_ocv = value.intval;
 
+	/* All current limits in charger */
+	
+	psy_do_property("sec-charger", get,
+		POWER_SUPPLY_PROP_CURRENT_AVG, value);
+	battery->current_avg = value.intval;
+
+	psy_do_property("sec-charger", get,
+		POWER_SUPPLY_PROP_CURRENT_NOW, value);
+	battery->current_now = value.intval;
+
+	psy_do_property("sec-charger", get,
+		POWER_SUPPLY_PROP_CURRENT_MAX, value);
+	battery->current_max = value.intval;
+
 	value.intval = SEC_BATTEY_CURRENT_MA;
 	psy_do_property("sec-fuelgauge", get,
 		POWER_SUPPLY_PROP_CURRENT_NOW, value);
@@ -2472,33 +2487,6 @@ ssize_t sec_bat_store_attrs(
 			ret = count;
 		}
 		break;
-#if defined(CONFIG_SAMSUNG_BATTERY_ENG_TEST)
-	case BATT_TEST_CHARGE_CURRENT:
-		if (sscanf(buf, "%d\n", &x) == 1) {
-			if (x >= 0 && x <= 2000) {
-				union power_supply_propval value;
-				dev_err(battery->dev,
-					"%s: BATT_TEST_CHARGE_CURRENT(%d)\n", __func__, x);
-				battery->pdata->charging_current[
-					POWER_SUPPLY_TYPE_USB].input_current_limit = x;
-				battery->pdata->charging_current[
-					POWER_SUPPLY_TYPE_USB].fast_charging_current = x;
-				if (x > 500) {
-					battery->pdata->temp_check_type =
-						SEC_BATTERY_TEMP_CHECK_NONE;
-					battery->pdata->charging_total_time =
-						10000 * 60 * 60;
-				}
-				if (battery->cable_type == POWER_SUPPLY_TYPE_USB) {
-					value.intval = x;
-					psy_do_property("sec-charger", set,
-						POWER_SUPPLY_PROP_POWER_NOW, value);
-				}
-			}
-			ret = count;
-		}
-		break;
-#endif
 	default:
 		ret = -EINVAL;
 	}
@@ -2710,6 +2698,9 @@ static int sec_bat_get_property(struct power_supply *psy,
 		break;
 	case POWER_SUPPLY_PROP_CURRENT_AVG:
 		val->intval = battery->current_avg;
+		break;
+	case POWER_SUPPLY_PROP_CURRENT_MAX:
+		val->intval = battery->current_max;
 		break;
 	/* charging mode (differ from power supply) */
 	case POWER_SUPPLY_PROP_CHARGE_NOW:
