65203f7be92a7caa175c0f6efdb187983962b028
yank555-lu <yank555.lu@gmail.com>
2014-01-19 19:34:48 +0100

    mm/swap: fix race on swap_info reuse between swapoff and swapon
    Date	Thu, 09 Jan 2014 13:39:55 +0800
    
    swapoff clear swap_info's SWP_USED flag prematurely and free its resources
    after that. A concurrent swapon will reuse this swap_info while its previous
    resources are not cleared completely.
    
    These late freed resources are:
    - p->percpu_cluster
    - swap_cgroup_ctrl[type]
    - block_device setting
    - inode->i_flags &= ~S_SWAPFILE
    
    This patch clear SWP_USED flag after all its resources freed, so that swapon
    can reuse this swap_info by alloc_swap_info() safely.
    
    Signed-off-by: Weijie Yang <weijie.yang@samsung.com>
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -1746,6 +1746,16 @@ SYSCALL_DEFINE1(swapoff, const char __user *, specialfile)
 		mutex_unlock(&inode->i_mutex);
 	}
 	filp_close(swap_file, NULL);
+
+	/*
+	* clear SWP_USED flag after all resources freed
+	* so that swapon can reuse this swap_info in alloc_swap_info() safely
+	* it is ok to not hold p->lock after we cleared its SWP_WRITEOK
+	*/
+	spin_lock(&swap_lock);
+	p->flags = 0;
+	spin_unlock(&swap_lock);
+
 	err = 0;
 	atomic_inc(&proc_poll_event);
 	wake_up_interruptible(&proc_poll_wait);
