04a2ed304e504d240a10133602132df9b38c7ce2
halaszk <halaszk@deathlord.eu>
2014-04-14 22:20:15 +0200

    exfat: AIO Optimization compatibility fixup

--- fs/exfat/exfat_super.c
+++ fs/exfat/exfat_super.c
@@ -1548,7 +1548,7 @@ static int exfat_write_end(struct file *file, struct address_space *mapping,
 	return err;
 }
 
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,15,0)
+#ifdef CONFIG_AIO_OPTIMIZATION
 static ssize_t exfat_direct_IO(int rw, struct kiocb *iocb, struct iov_iter *iter,
 				loff_t offset)
 #else
@@ -1564,7 +1564,7 @@ static ssize_t exfat_direct_IO(int rw, struct kiocb *iocb,
 	ssize_t ret;
 
 	if (rw == WRITE) {
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,15,0)
+#ifdef CONFIG_AIO_OPTIMIZATION
 		if (EXFAT_I(inode)->mmu_private < (offset + iov_iter_count(iter)))
 #else
 		if (EXFAT_I(inode)->mmu_private < (offset + iov_length(iov, nr_segs)))
@@ -1572,7 +1572,7 @@ static ssize_t exfat_direct_IO(int rw, struct kiocb *iocb,
 			return 0;
 	}
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(3,2,00)
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,15,0)
+#ifdef CONFIG_AIO_OPTIMIZATION
 	ret = blockdev_direct_IO(rw, iocb, inode, iter,
 					offset, exfat_get_block);
 #else
@@ -1586,7 +1586,7 @@ static ssize_t exfat_direct_IO(int rw, struct kiocb *iocb,
 
 #if LINUX_VERSION_CODE > KERNEL_VERSION(2,6,34)
 	if ((ret < 0) && (rw & WRITE))
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(3,15,0)
+#ifdef CONFIG_AIO_OPTIMIZATION
 		exfat_write_failed(mapping, offset+iov_iter_count(iter));
 #else
 		exfat_write_failed(mapping, offset+iov_length(iov, nr_segs));
