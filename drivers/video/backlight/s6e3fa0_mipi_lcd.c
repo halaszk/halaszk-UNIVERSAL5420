082f009c07ac987b9bcec29bf0792bf760e5faaf
halaszk <halaszk@deathlord.eu>
2014-05-05 14:49:51 +0200

    kernel/power/powersuspend: LCD screen on/off hooks (Yank555.lu)
    - add an alternative hook for screen on / off detection in the display
      pannel driver
    - sleeps  ~0.1s sooner, wakes up ~1s later then autosleep

--- drivers/video/backlight/s6e3fa0_mipi_lcd.c
+++ drivers/video/backlight/s6e3fa0_mipi_lcd.c
@@ -29,6 +29,9 @@
 #include <plat/dsim.h>
 #include <plat/mipi_dsi.h>
 #include <plat/gpio-cfg.h>
+#ifdef CONFIG_POWERSUSPEND
+#include <linux/powersuspend.h>
+#endif
 #include <asm/system_info.h>
 
 #include "s6e3fa0_param.h"
@@ -1966,6 +1969,10 @@ static int s6e3fa0_displayon(struct mipi_dsim_device *dsim)
 
 	s6e3fa0_power(lcd, FB_BLANK_UNBLANK);
 
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_pannel_hook(POWER_SUSPEND_INACTIVE); // Yank555.lu : add hook to handle powersuspend tasks (wakeup)
+#endif
+
 	return 0;
 }
 
@@ -1974,6 +1981,10 @@ static int s6e3fa0_suspend(struct mipi_dsim_device *dsim)
 	struct lcd_info *lcd = g_lcd;
 
 	s6e3fa0_power(lcd, FB_BLANK_POWERDOWN);
+
+#ifdef CONFIG_POWERSUSPEND
+	set_power_suspend_state_pannel_hook(POWER_SUSPEND_ACTIVE); // Yank555.lu : add hook to handle powersuspend tasks (sleep)
+#endif
 
 	return 0;
 }
